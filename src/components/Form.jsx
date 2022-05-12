cdfae4e1507a227c291abb05c138cb5fb7d1d336
Bradley-Bird <Bradleybird2@icloud.com>
2022-05-11 17:15:29 -0700

    basic form on main

---
+++ src/components/Form.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import { Container, Box, TextField, Button } from '@mui/material';
+const handleSubmit = (e) => {
+  e.preventDefault();
+};
+function Form() {
+  const [todo, setTodo] = useState('');
+  return (
+    <Container>
+      <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          id="add todo"
+          label="What do we want to get done today?"
+          value={todo}
+          onChange={(e) => setTodo(e.target.value)}
+        />
+        <Button
+          type="submit"
+          fullWidth
+          variant="contained"
+          sx={{ mt: 3, mb: 2 }}
+        >
+          Save
+        </Button>
+      </Box>
+    </Container>
+  );
+}
+
+export default Form;
--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './App';
+import { ThemeProvider, createTheme } from '@mui/material/';
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#2E4052',
+      light: '#E7ECF2',
+    },
+    secondary: {
+      main: '#FFC857',
+    },
+    background: { default: '#FFFFFF' },
+  },
+});
 
 render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
